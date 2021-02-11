d78cc11ff71c5465619c238ff080ed3cb96f765f
Victoria <niaba.victoria@gmail.com>
2021-02-11 20:44:33 +0100

    init: initialisation du projet Spring Boot
    Généré à partir de Spring Initializr : https://start.spring.io/#!type=maven-project&language=java&platformVersion=2.4.2.RELEASE&packaging=jar&jvmVersion=11&groupId=fr.univamu&artifactId=epu&name=Etude%20du%20parcours%20des%20etudiants&description=Back-end%20du%20projet%20EPU.&packageName=fr.univamu.epu&dependencies=devtools,configuration-processor,lombok,web,data-jpa,mysql,hsql

---
+++ src/test/java/fr/univamu/epu/EpuApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.univamu.epu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
