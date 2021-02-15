e0a6b380ab80c9ad26e91751ad29233ae45f13ad
FORESTIER Loic <loic.forestier@etu.univ-amu.fr>
2021-02-15 16:20:03 +0100

    Registration mock

--- src/app/components/registrations/registrations.component.ts
+++ src/app/components/registrations/registrations.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RegistrationService } from 'src/app/services/registration.service';
 
 @Component({
   selector: 'app-registrations',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private registrationService: RegistrationService) { }
+
+  totalRegistration = 0;
 
   ngOnInit(): void {
+    this.totalRegistration = this.registrationService.getAllRegistrations();
+  }
+
+  getNumberByYear(year:number):number {
+    return this.registrationService.getNumberOfRegistrationsByYear(year);
+  }
+
+  getYears() {
+    return this.registrationService.getAllYearAvailable();
+  }
+
+  getYearTimeStamp(year:number) {
+    return this.registrationService.getTimestampOfRegistrationByYear(year);
   }
 
 }
--- src/app/services/registration.service.ts
+++ src/app/services/registration.service.ts
@@ -1,28 +1,150 @@
 import { Injectable } from '@angular/core';
 
+
+/**/ 
+export class Registration {
+  ia_id: string;
+  year: number;
+  
+  constructor(ia_id:string, year:number) {
+      this.ia_id = ia_id;
+      this.year = year;
+  };
+}
+/**/
+
 @Injectable({
   providedIn: 'root'
 })
 export class RegistrationService {
-  all_registrations: number;
-  nb_registrations: number;
-  timestamp: Date;
 
-  constructor() { 
-    this.all_registrations = 81000;
-    this.nb_registrations = 13000;
-    this.timestamp = new Date();
-  }
+  REGISTRATION: Registration[] = [
+    new Registration("0",2011),
+    new Registration("1",2011),
+    new Registration("2",2011),
+    new Registration("3",2011),
+    new Registration("4",2011),
+    new Registration("4",2011),
+    new Registration("4",2011),
+    new Registration("4",2011),
+    new Registration("4",2011),
+    new Registration("4",2011),
+    new Registration("4",2011),
+    new Registration("4",2011),
+    new Registration("4",2011),
+    new Registration("4",2011),
+    new Registration("4",2011),
+    new Registration("4",2011),
+    new Registration("4",2011),
+    new Registration("4",2011),
+    new Registration("4",2011),
+    new Registration("4",2011),
+    new Registration("4",2011),
+    new Registration("4",2011),
+    new Registration("4",2011),
+    new Registration("4",2011),
+    new Registration("4",2011),
+    new Registration("4",2011),
+    new Registration("5",2012),
+    new Registration("6",2012),
+    new Registration("7",2012),
+    new Registration("7",2012),
+    new Registration("7",2012),
+    new Registration("7",2012),
+    new Registration("7",2012),
+    new Registration("7",2012),
+    new Registration("7",2012),
+    new Registration("7",2012),
+    new Registration("7",2012),
+    new Registration("7",2012),
+    new Registration("7",2012),
+    new Registration("7",2012),
+    new Registration("7",2012),
+    new Registration("7",2012),
+    new Registration("7",2012),
+    new Registration("7",2012),
+    new Registration("7",2012),
+    new Registration("7",2012),
+    new Registration("7",2012),
+    new Registration("7",2012),
+    new Registration("7",2012),
+    new Registration("7",2012),
+    new Registration("7",2012),
+    new Registration("7",2012),
+    new Registration("7",2012),
+    new Registration("7",2012),
+    new Registration("7",2012),
+    new Registration("7",2012),
+    new Registration("7",2012),
+    new Registration("7",2012),
+    new Registration("7",2012),
+    new Registration("7",2012),
+    new Registration("7",2012),
+    new Registration("8",2012),
+    new Registration("9",2012),
+    new Registration("10",2013),
+    new Registration("11",2013),
+    new Registration("12",2013),
+    new Registration("12",2013),
+    new Registration("12",2013),
+    new Registration("12",2013),
+    new Registration("12",2013),
+    new Registration("12",2013),
+    new Registration("12",2013),
+    new Registration("12",2013),
+    new Registration("12",2013),
+    new Registration("12",2013),
+    new Registration("12",2013),
+    new Registration("12",2013),
+    new Registration("12",2013),
+    new Registration("12",2013),
+    new Registration("12",2013),
+    new Registration("12",2013),
+    new Registration("12",2013),
+    new Registration("12",2013),
+    new Registration("12",2013),
+    new Registration("12",2013),
+    new Registration("12",2013),
+    new Registration("12",2013),
+    new Registration("12",2013),
+    new Registration("12",2014),
+    new Registration("12",2015),
+    new Registration("12",2016),
+    new Registration("12",2017),
+    new Registration("12",2018),
+    new Registration("12",2013),
+    new Registration("13",2013),
+    new Registration("14",2013),
+    new Registration("15",2013)
+  ];
+
+  constructor() { }
 
   getAllRegistrations():number{
-    return this.all_registrations;
+    return this.REGISTRATION.length;
   }
 
   getNumberOfRegistrationsByYear(year: number):number{
-    return this.nb_registrations;
+    let count = 0;
+    this.REGISTRATION.forEach(registration => {
+      if(registration.year == year) {
+        count++;
+      }      
+    });
+    return count;
   }
 
   getTimestampOfRegistrationByYear(year: number):Date{
-    return this.timestamp;
+    return new Date();
+  }
+
+  getAllYearAvailable() {
+    let years = new Array<number>();
+    this.REGISTRATION.forEach(registration => {
+      if(!years.includes(registration.year)) {
+        years.push(registration.year);
+      }
+    });
+    return years;
   }
 }
