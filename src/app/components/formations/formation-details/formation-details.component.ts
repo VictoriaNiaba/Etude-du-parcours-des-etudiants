ada2cb46b5502f99d386f29f9628be3a56edfe9b
CABOUAT Charlotte <charlotte.cabouat@etu.univ-amu.fr>
2021-02-15 15:09:43 +0100

    Page de détail des formations

--- src/app/components/formations/formation-details/formation-details.component.ts
+++ src/app/components/formations/formation-details/formation-details.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Formation } from 'src/app/models/formation';
+import { User } from 'src/app/models/User';
+import { AuthentificationService } from 'src/app/services/authentification.service';
+import { FormationService } from 'src/app/services/formation.service';
 
 @Component({
   selector: 'app-formation-details',
@@ -6,10 +11,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./formation-details.component.scss']
 })
 export class FormationDetailsComponent implements OnInit {
+  formation: Formation;
+  canEdit: boolean;
+  currentUser: User;
 
-  constructor() { }
+  constructor(private formationService: FormationService, private route: ActivatedRoute,private authenticationService: AuthentificationService) { }
 
   ngOnInit(): void {
+    this.formation = this.formationService.getByCode(this.route.snapshot.paramMap.get('id'));
+    this.canEdit=this.setEdit();
+    this.currentUser = this.authenticationService.currentUser;
   }
 
+  setEdit():boolean{
+    /*
+    if(this.currentUser.role==="ADMIN") return true;
+    else return false;
+    */
+   return true;
+  }
 }
