00cc53f9bf92a4db506f39bceab936930197e778
CABOUAT Charlotte <charlotte.cabouat@etu.univ-amu.fr>
2021-02-16 11:59:17 +0100

    edition d'une formation works

--- src/app/components/formations/formation-edit/formation-edit.component.ts
+++ src/app/components/formations/formation-edit/formation-edit.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Formation } from 'src/app/models/formation';
+import { FormationService } from 'src/app/services/formation.service';
 
 @Component({
   selector: 'app-formation-edit',
@@ -7,9 +11,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FormationEditComponent implements OnInit {
 
-  constructor() { }
+  formation: Formation;
+  editForm: FormGroup;
+  isAddMode: Boolean;
+  submitted = false;
+  id: string;
+
+  constructor(private formationService: FormationService, private route: ActivatedRoute, private router: Router, private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.id = this.route.snapshot.paramMap.get('id');
+    if(this.id === null) {this.isAddMode = true;}
+    else {this.isAddMode = false;}
+    this.editForm = this.formBuilder.group({
+      formation_code: ['', [Validators.required]],
+      formation_name: ['', [Validators.required]],
+      description: ['', Validators.required],
+      type: ['', [Validators.required]],
+      url: ['', [Validators.required]],
+    });
+    if (!this.isAddMode) {
+      this.formation = this.formationService.getByCode(this.id)
+      this.editForm.setValue({formation_code:this.formation.formation_code,formation_name:this.formation.formation_name, description:this.formation.description, type:this.formation.type,url:this.formation.url});
+    }
+    else{
+    }
+  }
+
+  get formControl() { return this.editForm.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+
+    if (this.editForm.invalid) {
+        return;
+    }
+
+    if (this.isAddMode) {
+        this.createSite();
+    } else {
+        this.updateSite();
+    }
+  }
+
+  private createSite() {
+    let newId = this.formationService.create(this.editForm.value);
+    this.router.navigate(['/formation',newId]);
+  }
+
+  private updateSite() {
+    this.formationService.update(this.id, this.editForm.value);
+    this.router.navigate(['/formation',this.editForm.value.formation_code]);
   }
 
 }
