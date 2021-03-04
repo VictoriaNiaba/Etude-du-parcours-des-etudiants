44d44506b9aa7cd68904458d0ada377affd7711e
Victoria <niaba.victoria@gmail.com>
2021-03-04 22:37:05 +0100

    fix(config): utilisation de spring boot pour le scan des composants

--- src/main/java/fr/univamu/epu/Starter.java
+++ src/main/java/fr/univamu/epu/Starter.java
@@ -2,13 +2,8 @@ package fr.univamu.epu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 
-import fr.univamu.epu.business.SpringBusinessConfig;
-import fr.univamu.epu.dao.SpringDAOConfig;
-
-@SpringBootApplication
-@Import({SpringDAOConfig.class, SpringBusinessConfig.class })
+@SpringBootApplication	
 public class Starter {
 
 	public static void main(String[] args) {
