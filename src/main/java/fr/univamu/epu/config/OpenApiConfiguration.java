a3a8340b1c57fda236cfb6c9da53ec73fd438a6a
Victoria <niaba.victoria@gmail.com>
2021-03-15 21:15:40 +0100

    feat: configuration de la sécurité

--- src/main/java/fr/univamu/epu/config/OpenApiConfiguration.java
+++ src/main/java/fr/univamu/epu/config/OpenApiConfiguration.java
@@ -1,4 +1,4 @@
-package fr.univamu.epu.openapi;
+package fr.univamu.epu.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
---
+++ src/main/java/fr/univamu/epu/config/SecurityConfiguration.java
@@ -0,0 +1,27 @@
+package fr.univamu.epu.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+
+@Configuration
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/favicon.ico", "/*.js", "/*.css");
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.cors().and()
+				.httpBasic().and()
+				.authorizeRequests()
+				.antMatchers("/index.html", "/", "/home", "/login")
+				.permitAll()
+				.anyRequest().authenticated()
+				.and().csrf()
+				.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+	}
+}
